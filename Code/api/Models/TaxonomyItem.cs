f62d475498985d83e769626f03d0415050f2eb87
Oscar Marin <oscar@marin.cr>
2016-12-26 15:03:12 -0600

    added new dto

---
+++ Code/api/Models/TaxonomyItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class TaxonomyItem
+    {
+        public bool IsConfident { get; set; }
+        public double Score { get; set; }
+        public string Label { get; set; }
+    }
+}
