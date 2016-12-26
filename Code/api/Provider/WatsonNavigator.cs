5dce8d1973179874fb8858b20b15ea7990522a86
Oscar Marin <oscar@marin.cr>
2016-12-26 13:30:49 -0600

    added new classes

---
+++ Code/api/Provider/WatsonNavigator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Provider
+{
+    public class WatsonNavigator
+    {
+        private string GetApiKey()
+        {
+            return "";
+        }
+    }
+}
