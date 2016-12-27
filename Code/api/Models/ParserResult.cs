cbbb0ed4dbcfa31f803b948fa4cbd999d78c0352
Oscar Marin <oscar@marin.cr>
2016-12-26 20:43:35 -0600

    Added new fields to the result set

--- Code/api/Models/ParserResult.cs
+++ Code/api/Models/ParserResult.cs
@@ -14,6 +14,7 @@ namespace api.Models
             Id = Guid.NewGuid().ToString();
         }
         public string Id { get; set; }
+        public string UserId { get; set; }
         public string Title { get; set; }
         public string TitleHash { get; set; }
         public string Content { get; set; }
@@ -22,6 +23,7 @@ namespace api.Models
         public string DatePublish { get; set; }
         public string ImageUrl { get; set; }
         public string Url { get; set; }
+        public IEnumerable<string> Images { get; set; }
         public IEnumerable<string> Keywords { get; set; }
         public IEnumerable<string> Categories { get; set; }
         public DateTime UtcCreatedOn { get; set; }
