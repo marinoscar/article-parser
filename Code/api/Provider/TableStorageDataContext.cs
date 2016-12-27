55a9e55e5c741d3cef8e3e70235e9624427a36cc
Oscar Marin <oscar@marin.cr>
2016-12-26 21:38:04 -0600

    Added support to persist the information

---
+++ Code/api/Provider/TableStorageDataContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Provider
+{
+    public class TableStorageDataContext
+    {
+        CloudStorageAccount _account;
+        CloudTableClient _tableClient;
+
+        public TableStorageDataContext()
+        {
+            _account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["azure.storage.key"]);
+            _tableClient = _account.CreateCloudTableClient();
+        }
+
+        public void Insert<T>(string table, IEnumerable<T> entities) where T : ITableEntity
+        {
+            var tableRef = _tableClient.GetTableReference(table);
+            var operation = new TableBatchOperation();
+            foreach(var e in entities)
+            {
+                operation.InsertOrReplace(e);
+            }
+            tableRef.ExecuteBatch(operation);
+        }
+    }
+}
