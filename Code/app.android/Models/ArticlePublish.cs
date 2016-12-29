e59e95ad92e77b4429d700880e88e8742cf144e1
Oscar Marin <oscar@marin.cr>
2016-12-29 14:20:24 -0600

    added the logic to post the article

---
+++ Code/app.android/Models/ArticlePublish.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace app.android.Models
+{
+    public class ArticlePublish
+    {
+        public string Url { get; set; }
+        public bool Wordpress { get; set; }
+        public bool Facebook { get; set; }
+        public bool Twitter { get; set; }
+        public bool LinkedIn { get; set; }
+        public bool Buffer { get; set; }
+
+        public string ToJson()
+        {
+            var template = @"
+{
+	""Parse"":{
+		""Url"": ""{0}""
+	},
+	""Article"":{
+		""PostStatus"": ""{1}""
+	},
+	""SocialMedia"":{
+		""DoFacebook"": {2},
+		""DoTwitter"": {3},
+		""DoLinkedIn"": {4},
+		""Buffer"": {5}
+	}
+}
+";
+            return string.Format(template, Url,
+                Wordpress ? "publish" : "draft",
+                Facebook, Twitter, LinkedIn, Buffer);
+        }
+    }
+}
--- Code/app.android/SaveArticle.cs
+++ Code/app.android/SaveArticle.cs
@@ -9,6 +9,8 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using app.android.Models;
+using RestSharp;
 
 namespace app.android
 {
@@ -21,5 +23,49 @@ namespace app.android
             SetContentView(Resource.Layout.SaveArticle);
             
         }
+
+        private void SetupEvents()
+        {
+            var publishBtn = FindViewById<Button>(Resource.Id.publishBtn);
+            publishBtn.Click += PublishBtn_Click;
+        }
+
+        private void PublishBtn_Click(object sender, EventArgs e)
+        {
+            PostData();
+            Toast.MakeText(this, "PUBLISHED", ToastLength.Short).Show();
+            GoToMain();
+        }
+
+        private void GoToMain()
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+        }
+
+        private void PostData()
+        {
+            var payload = GetModel().ToJson();
+            var client = new RestClient("http://k-app.azurewebsites.net/api/publish/persist");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("token", this.GetToken());
+            request.AddHeader("content-type", "application/json");
+            request.AddParameter("application/json", payload, ParameterType.RequestBody);
+            var response = client.Execute(request);
+        }
+
+        private ArticlePublish GetModel()
+        {
+            var result = new ArticlePublish()
+            {
+                Url = FindViewById<EditText>(Resource.Id.urlTxt).Text,
+                Wordpress = FindViewById<CheckBox>(Resource.Id.wpChk).Checked,
+                Facebook = FindViewById<CheckBox>(Resource.Id.fbCheck).Checked,
+                Twitter = FindViewById<CheckBox>(Resource.Id.twitterChk).Checked,
+                LinkedIn = FindViewById<CheckBox>(Resource.Id.linkedInChk).Checked
+            };
+            return result;
+        } 
+
     }
 }
