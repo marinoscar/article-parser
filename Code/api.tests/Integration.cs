57f7585360dd4b91e7c99def0bd9c3aa12f5376d
Oscar Marin <oscar@marin.cr>
2016-12-28 21:16:47 -0600

    added a test class and missing references

--- Code/api.tests/Integration.cs
+++ Code/api.tests/Integration.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using api.core.Provider;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,13 @@ using System.Threading.Tasks;
 
 namespace api.tests
 {
+    [TestFixture]
     public class Integration
     {
-
+        [Test]
+        public void DoFullIntegration()
+        {
+            var publishManager = new PublishManager();
+        }
     }
 }
